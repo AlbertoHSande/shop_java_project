d3e0bae3b780cdaf98f3ca572b93a0870f67cc45
Alberto Hernández <alberto.hernandez@hdsettings.com>
2017-10-09 11:44:37 +0200

    servlet modificado

--- videogameShop/src/services/servlet_producto.java
+++ videogameShop/src/services/servlet_producto.java
@@ -1,10 +1,13 @@
 package services;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import DAOInterface;
 
 @WebServlet("/producto")
 
@@ -15,8 +18,18 @@ public class servlet_producto extends HttpServlet {
 	 */
 	private static final long serialVersionUID = -6251665846157031729L;
 
-	public void sProdcuto(HttpServletRequest request, HttpServletResponse response) throws ServletException {
-		
+	public void sProducto(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+		try {
+			String operation = request.getParameter("operation");
+			DAOInterface dao = new DAOProducto();
+			
+			if(operation.equals("alta")) {
+				dao.
+			}
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getStackTrace());
+		}
+
 	}
 
 }
